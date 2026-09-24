c982e4b618e548f1b88f1b24727b7d2343b21931
anton.tarasyuk213 <anton.tarasyuk213@gmail.com>
2026-09-24 16:46:30 +0300

    Добавьте файлы проекта.

---
+++ GreenLIne/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GreenLIne
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
